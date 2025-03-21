c9a1a2dae780e0ac437d8232714fef2c551fe28c
wilfrid <wilfrid.ndongmodjoumessi@fanuc.eu>
2025-03-21 12:17:02 +0100

    creation de vue pour afficher les stats de chaque joueur y compris le temps moyen de prise de decision

--- wv_views.sql
+++ wv_views.sql
@@ -45,19 +45,52 @@
 
 -- Vue 3 : ALL_PLAYERS_ELAPSED_TOUR
 -- Affiche le temps moyen de chaque prise de décision par tour.
-CREATE VIEW ALL_PLAYERS_ELAPSED_TOUR AS
+-- CREATE VIEW ALL_PLAYERS_ELAPSED_TOUR AS
+-- SELECT 
+--     p.pseudo AS nom_joueur,
+--     pt.title_party AS nom_partie,
+--     t.id_turn AS numero_tour,
+--     t.start_time AS debut_tour,
+--     pp.start_time AS prise_decision,
+--     DATEDIFF(SECOND, t.start_time, pp.start_time) AS secondes_dans_le_tour
+-- FROM 
+--     players AS p
+-- JOIN 
+--     players_play AS pp ON p.id_player = pp.id_player
+-- JOIN 
+--     turns AS t ON pp.id_turn = t.id_turn
+-- JOIN 
+--     parties AS pt ON t.id_party = pt.id_party;
+
+
+-- Vue 4 : ALL_PLAYERS_STATS
+-- Affiche les statistiques de chaque joueur, y compris le temps moyen de prise de décision.
+CREATE VIEW ALL_PLAYERS_STATS AS
 SELECT 
     p.pseudo AS nom_joueur,
+    r.description_role AS role,
     pt.title_party AS nom_partie,
-    t.id_turn AS numero_tour,
-    t.start_time AS debut_tour,
-    pp.start_time AS prise_decision,
-    DATEDIFF(SECOND, t.start_time, pp.start_time) AS secondes_dans_le_tour
+    COUNT(pp.id_turn) AS nb_tours_joues,
+    (
+        SELECT COUNT(*) 
+        FROM turns 
+        WHERE id_party = pt.id_party
+    ) AS nb_total_tours,
+    CASE
+        WHEN r.description_role = 'loup' AND pip.is_alive = 0 THEN 'loups'
+        WHEN r.description_role = 'villageois' AND pip.is_alive = 1 THEN 'villageois'
+        ELSE 'aucun'
+    END AS vainqueur,
+    AVG(DATEDIFF(SECOND, pp.start_time, pp.end_time)) AS temps_moyen_decision
 FROM 
     players AS p
 JOIN 
+    players_in_parties AS pip ON p.id_player = pip.id_player
+JOIN 
+    roles AS r ON pip.id_role = r.id_role
+JOIN 
+    parties AS pt ON pip.id_party = pt.id_party
+JOIN 
     players_play AS pp ON p.id_player = pp.id_player
-JOIN 
-    turns AS t ON pp.id_turn = t.id_turn
-JOIN 
-    parties AS pt ON t.id_party = pt.id_party;
+GROUP BY 
+    p.pseudo, r.description_role, pt.title_party, pip.is_alive, pt.id_party;
